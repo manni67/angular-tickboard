5151bbcc9a21d7cb30f07d152e9277a60f941e7e
Manfred <manfred.hennig@t-online.de>
2022-09-03 22:03:12 +0200

    refactor to central used menu

--- tickboard/src/app/app.module.ts
+++ tickboard/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingpageComponent
+    LandingpageComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
