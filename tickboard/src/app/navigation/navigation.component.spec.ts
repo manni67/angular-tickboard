35c33f3be5767c110a64a4ea57590e91c5f87af1
Manfred <manfred.hennig@t-online.de>
2023-03-12 14:24:43 +0100

    spec.ts commented

--- tickboard/src/app/navigation/navigation.component.spec.ts
+++ tickboard/src/app/navigation/navigation.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavigationComponent } from './navigation.component';
 
+// generated for test
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
   let fixture: ComponentFixture<NavigationComponent>;
